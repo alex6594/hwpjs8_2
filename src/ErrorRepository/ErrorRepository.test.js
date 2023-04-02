91eb7a62ac2510e046feab967a69fa0f54c46fdb
unknown <alex6594@mail.ru>
2023-04-02 16:00:42 +0300

    initial

---
+++ src/ErrorRepository/ErrorRepository.test.js
@@ -0,0 +1,15 @@
+import ErrorRepository from './ErrorRepository.js';
+
+const Err = new ErrorRepository();
+
+test('should return human-readable text of error (1)', () => {
+  expect(Err.translate(1)).toBe('first error');
+});
+
+test('should return human-readable text of error (2)', () => {
+  expect(Err.translate(2)).toBe('second error');
+});
+
+test('should return text unkown error if no code)', () => {
+  expect(Err.translate(5)).toBe('Unknown error');
+});
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// entry point for Webpack
+import './ErrorRepository/ErrorRepository.js';
